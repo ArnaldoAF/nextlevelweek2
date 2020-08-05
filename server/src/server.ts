d3f70bcc23a840a8a5909568b113f990e2678195
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2020-08-05 19:01:12 -0300

    Server: Created Database

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import routes from './routes';
 
 const app = express();
 
-app.use(express.json()); //para o express entender JSON(ajuda a reconhecer o body)
+app.use(express.json()); //para o express entender JSON(ajuda a reconhecer o body)yarn add knex sq
 app.use(routes);
 
 
