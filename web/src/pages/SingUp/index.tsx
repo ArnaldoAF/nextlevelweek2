bda3bd10ff84c5042431e6bb6aa8cb6bd7dc3935
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2020-09-01 13:18:01 -0300

    Tela de Cadastro pronta

--- web/src/pages/SingUp/index.tsx
+++ web/src/pages/SingUp/index.tsx
@@ -8,24 +8,18 @@ import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import Checkbox from '../../components/Checkbox';
 import MaterialInput from '../../components/MaterialInput';
+import backIcon from '../../assets/images/icons/back.svg';
 
-const Login:React.FC = () => {
+const SingUp: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [remember, setRemember] = useState("false");
-    
     return (
         <>
         <div id="content">
-            <div id="logo-area">
-                
-                <div id="logo-container">
-                    
-                    <img src={LogoImg} alt=""/>
-                    <h2 >Sua plataforma de <br /> estudos online</h2>
-                </div>
-                
-            </div>
+            
             <div id="logo-area-mobile">
                 <div id="logo-container">
                     <img src={LogoImg} alt=""/>
@@ -38,8 +32,26 @@ const Login:React.FC = () => {
                 
 
                 <form className="form-login"> 
-                    <h1>Fazer Login</h1>
-                    <div>
+                    <Link to="/">
+                        <img src={backIcon} alt=""/>
+                    </Link>
+                    <div className="partial-form">
+                    <h1>Cadastro</h1>
+                    <h3>Preencha os dados abaixo <br /> para começar</h3>
+                    <div > 
+                    <MaterialInput
+                            name="name"
+                            label="Nome"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+
+                        <MaterialInput
+                            name="lastname"
+                            label="SobreNome"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
                         <MaterialInput
                             name="email"
                             label="E-mail"
@@ -57,35 +69,32 @@ const Login:React.FC = () => {
                         </div>
 
 
-                        <div className="password-options">
-                        <Checkbox
-                            name="remember"
-                            label="Lembrar-me"
-                            value="{remember}"
-                            onChange={(e) => setRemember(e.target.value)}
-                            type="checkbox"
-                        />
-                        <Link to="/" className="forgot-password">Esqueci minha senha</Link>
+                        
 
 
-                        </div> 
+                        
 
-                        <Button type="submit"> Entrar</Button>
+                        <Button type="submit"> Cadastrar</Button>
+                        </div>
 
                         
 
 
                 </form>
-                <div className="login-footer">
-                            <p id="cadastrar">Não tem conta? <br /> 
-                            <Link to="/singup">Cadastre-se</Link>
-                            </p>
-                            <p id="text-free">É de graça ❤</p>
-                        </div>
+                
+            </div>
+            <div id="logo-area">
+                
+                <div id="logo-container">
+                    
+                    <img src={LogoImg} alt=""/>
+                    <h2 >Sua plataforma de <br /> estudos online</h2>
+                </div>
+                
             </div>
         </div>
         </>
-    );
+    )
 }
- 
-export default Login;
+
+export default SingUp;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -5,12 +5,14 @@ import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 import Login from './pages/Login';
+import SingUp from './pages/SingUp';
 
 
 function Router() {
     return(
         <BrowserRouter>
             <Route path="/" component={Login} exact/>
+            <Route path="/singup" component={SingUp} />
             <Route path="/home" component={Landing} />
             <Route path="/study" component={TeacherList}/>
             <Route path="/give-classes" component={TeacherForm}/> 
