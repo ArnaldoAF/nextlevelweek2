8e4719dca3590e1f7e82fbe81d2c840411634f9d
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2020-09-14 16:50:53 -0300

    ROTAS PRIVADAS

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Redirect, RouteProps, Switch} from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
@@ -7,17 +7,42 @@ import TeacherForm from './pages/TeacherForm';
 import Login from './pages/Login';
 import SingUp from './pages/SingUp';
 import FullScreen from './components/FullScreen';
+import { isAutheticated } from './services/auth';
 
+interface PrivateRouteProps extends RouteProps {
+    // tslint:disable-next-line:no-any
+    component: any;
+}
+
+const PrivateRoute = (props:PrivateRouteProps) => {
+    const { component: Component, ...rest} = props;
+
+    return (
+        <Route 
+            {...rest}
+            render = { routerProps => 
+                
+                isAutheticated() ? ( 
+                    <Component {...routerProps} />
+                ) : (
+                    <Redirect to={{pathname:"/", state: { from: routerProps.location}}} />
+                )
+            }
+        />
+    )
+}
 
 function Router() {
     return(
         <BrowserRouter>
-            <Route path="/" component={Login} exact/>
-            <Route path="/singup" component={SingUp} />
-            <Route path="/home" component={Landing} />
-            <Route path="/study" component={TeacherList}/>
-            <Route path="/give-classes" component={TeacherForm}/> 
-            <Route path="/fullscreen" component={FullScreen}/> 
+            <Switch>
+                <Route path="/" component={Login} exact/>
+                <Route path="/singup" component={SingUp} />
+                <PrivateRoute path="/home" component={Landing} />
+                <PrivateRoute path="/study" component={TeacherList}/>
+                <PrivateRoute path="/give-classes" component={TeacherForm}/> 
+                <Route path="*" component={() => <h1>Page not found</h1>} />
+            </Switch>
         </BrowserRouter>
     )
 }
